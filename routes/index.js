91b1c4b61fa06150b85b423225a2d709dcced872
trionell <gustav.rakeberg@gmail.com>
2021-02-05 15:41:11 +0100

    update title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AzureLab' });
+  res.render('index', { title: 'AzureContainerLab' });
 });
 
 module.exports = router;
